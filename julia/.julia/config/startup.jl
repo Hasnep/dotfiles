b95d165e4871e5a45e5d441b8daa8720183efd24
Hannes <hasnep@gmail.com>
2020-05-11 21:52:57 +0100

    Remove ohmyrepl in julia startup

--- julia/.julia/config/startup.jl
+++ julia/.julia/config/startup.jl
@@ -1,10 +1,10 @@
 push!(LOAD_PATH, "/home/hannes/Documents/julia/")
 
-try
-    using OhMyREPL 
-catch e 
-    @warn(e.msg)
-end
+# try
+#     using OhMyREPL 
+# catch e 
+#     @warn(e.msg)
+# end
 
 try
     using Revise 
