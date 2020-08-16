8ff78e2bd7654374b46a3bd5a5c8726bcee71195
Hannes <hasnep@gmail.com>
2020-08-16 22:51:37 +0100

    Move aliases and functions into one file

--- .config/fish/functions/functions.fish
+++ .config/fish/functions/functions.fish
@@ -41,3 +41,39 @@ function smart-add-ppa -a "PPA_URL" -d "Remove a PPA and re-add it"
     echo "Adding $PPA_URL"
     sudo add-apt-repository -y $PPA_URL
 end
+
+# Aliases
+
+# Dotfiles
+alias dotfiles="/usr/bin/git --git-dir=$HOME/.dotfiles --work-tree=$HOME"
+complete --command dotfiles --wraps git
+
+# Commandline tools
+alias apt="aptitude"
+alias grep="grep --color=auto"
+alias ls="ls --color=auto --group-directories-first"
+alias please="sudo"
+
+# Python commandline tools
+alias glances="~/.venvs/glances/bin/python -m glances"
+alias jupyterlab="~/.venvs/jupyter/bin/python -m jupyter lab"
+alias jupytext="~/.venvs/jupytext/bin/jupytext"
+
+# Python tooling
+alias autopep8="~/.venvs/autopep8/bin/python -m autopep8"
+alias bandit="~/.venvs/bandit/bin/python -m bandit"
+alias black="~/.venvs/black/bin/python -m black"
+alias flake8="~/.venvs/flake8/bin/python -m flake8"
+alias mypy="~/.venvs/mypy/bin/python -m mypy"
+alias pip-compile="~/.venvs/pip-tools/bin/python -m piptools compile"
+alias prospector="~/.venvs/prospector/bin/python -m prospector"
+alias pycodestyle="~/.venvs/pycodestyle/bin/python -m pycodestyle"
+alias pydocstyle="~/.venvs/pydocstyle/bin/python -m pydocstyle"
+alias pylama="~/.venvs/pylama/bin/python -m pylama"
+alias pylint="~/.venvs/pylint/bin/python -m pylint"
+alias pytest="~/.venvs/pytest/bin/python -m pytest"
+alias yapf="~/.venvs/yapf/bin/python -m yapf"
+
+# Julia commandline tools
+alias pluto="julia --project -e 'import Pluto; Pluto.run()'"
+alias jlfmt="julia -e 'import JuliaFormatter; JuliaFormatter.format(\".\")'"
