09ee886d2628237aacc4dda91f5b0e4924a9ee47
Hannes <hasnep@gmail.com>
2020-08-19 15:27:53 +0100

    Fix setting syntax in pip functions

--- .config/fish/functions/functions.fish
+++ .config/fish/functions/functions.fish
@@ -18,7 +18,7 @@ function create-venv -a "VENV" "PACKAGES" -d "create-venv <venv> <packages>"
 end
 
 function pip-recompile -a "VENV" -d "Recompile requirements for a pip venv in ~/.venvs/<venv>"
-    set set VENV_PATH $HOME/.venvs/$VENV $HOME/.venvs/$VENV
+    set VENV_PATH $HOME/.venvs/$VENV
     echo "Compiling requirements"
     $VENV_PATH/bin/python -m pip install pip-tools
     $VENV_PATH/bin/python -m piptools compile "requirements.in"
@@ -27,12 +27,12 @@ function pip-recompile -a "VENV" -d "Recompile requirements for a pip venv in ~/
 end
 
 function pip-recreate -a "VENV" -d "Delete and recompile a pip venv in ~/.venvs/<venv>"
-    set set VENV_PATH $HOME/.venvs/$VENV $HOME/.venvs/$VENV
+    set VENV_PATH $HOME/.venvs/$VENV
     echo "Removing venv at $VENV_PATH"
     rm -rf $VENV_PATH
     echo "Creating new venv at $VENV_PATH"
     python3 -m venv $VENV_PATH
-    pip-recompile $VENV_NAME
+    pip-recompile $VENV
 end
 
 function smart-add-ppa -a "PPA_URL" -d "Remove a PPA and re-add it"
