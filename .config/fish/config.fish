da6712cb3b41ae9c854459a95596ce1b2b440c34
Hannes <hasnep@gmail.com>
2020-08-21 10:27:04 +0100

    Add onefetch if in vscode and in a git repo

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,1 +1,9 @@
-neofetch
+begin
+    set -l terminal_emulator (ps -p (ps -p $fish_pid -o ppid= | tr -d \[:space:\]) -o args=)
+    switch $terminal_emulator
+        case "tilix"
+            neofetch
+        case "*code*"
+            git rev-parse --git-dir &>/dev/null && onefetch
+    end
+end
