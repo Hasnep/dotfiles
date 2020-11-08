af7c7516a55fbd0d4e9534e595e438b6b0a78d85
Hannes <hasnep@gmail.com>
2020-11-08 12:18:48 +0000

    Add update-venvs function

--- .config/fish/functions/functions.fish
+++ .config/fish/functions/functions.fish
@@ -35,6 +35,25 @@ function pip-recreate -a "VENV" -d "Delete and recompile a pip venv in ~/.venvs/
     pip-recompile $VENV
 end
 
+function update-venvs -d "Update all my custom venvs"
+    create-venv awscli
+    create-venv bpytop
+    create-venv glances
+    create-venv jill
+    create-venv jupyter jupyter jupyterlab
+    create-venv jupytext jupyter jupytext
+    create-venv proselint
+
+    # Dev tools
+    create-venv black
+    create-venv flake8
+    create-venv mypy
+    create-venv pip-tools
+    create-venv pydocstyle
+    create-venv pytest
+    create-venv safety
+end
+
 function smart-add-ppa -a "PPA_URL" -d "Remove a PPA and re-add it"
     echo "Removing $PPA_URL"
     sudo add-apt-repository --remove -y $PPA_URL
@@ -63,20 +82,13 @@ alias jupytext="~/.venvs/jupytext/bin/jupytext"
 alias proselint="~/.venvs/proselint/bin/python -m proselint"
 
 # Python tooling
-alias autopep8="~/.venvs/autopep8/bin/python -m autopep8"
-alias bandit="~/.venvs/bandit/bin/python -m bandit"
 alias black="~/.venvs/black/bin/python -m black"
 alias flake8="~/.venvs/flake8/bin/python -m flake8"
 alias mypy="~/.venvs/mypy/bin/python -m mypy"
 alias pip-compile="~/.venvs/pip-tools/bin/python -m piptools compile"
-alias prospector="~/.venvs/prospector/bin/python -m prospector"
-alias pycodestyle="~/.venvs/pycodestyle/bin/python -m pycodestyle"
 alias pydocstyle="~/.venvs/pydocstyle/bin/python -m pydocstyle"
-alias pylama="~/.venvs/pylama/bin/python -m pylama"
-alias pylint="~/.venvs/pylint/bin/python -m pylint"
 alias pytest="~/.venvs/pytest/bin/python -m pytest"
 alias safety="~/.venvs/safety/bin/python -m safety"
-alias yapf="~/.venvs/yapf/bin/python -m yapf"
 
 # Julia commandline tools
 alias pluto="julia --project -e 'import Pkg; Pkg.update(\"Pluto\"); import Pluto; Pluto.run()'"
