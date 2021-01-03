6b1a997bbe417ce5b4586d5dd3648cdfec6c8e5d
Hannes <hasnep@gmail.com>
2021-01-03 20:09:12 +0000

    Change syntax of restic excludes file

--- .local/bin/restic-excludes-compile.py
+++ .local/bin/restic-excludes-compile.py
@@ -3,10 +3,12 @@ import json
 from pathlib import Path
 
 parser = argparse.ArgumentParser()
+parser.add_argument("--output", type=Path)
 parser.add_argument("input_file_path", type=Path)
 
 # Parse arguments
 args = parser.parse_args()
+output_file_path = args.output
 input_file_path = args.input_file_path
 
 if input_file_path is None:
@@ -15,6 +17,7 @@ if input_file_path is None:
     exit()
 
 # Read JSON
+print(f"Reading exclude source file from '{input_file_path}'.")
 with open(input_file_path, "r") as f:
     excludes = json.load(f)
 
@@ -24,4 +27,9 @@ for category, exclude_paths in excludes.items():
     output += f"# {category}\n"
     for exclude_path in exclude_paths:
         output += f"{exclude_path}\n"
-print(output)
+
+print(f"Writing exclude file to '{output_file_path}'.")
+with open(output_file_path, "w") as f:
+    f.write(output)
+
+print("Done converting excludes file.")
