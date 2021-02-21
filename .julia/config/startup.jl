69e44eef16df4f3a37a7c287f739f4dafdf751ca
Hannes <hasnep@gmail.com>
2021-02-21 10:58:09 +0000

    Remove commented code

--- .julia/config/startup.jl
+++ .julia/config/startup.jl
@@ -1,14 +1,7 @@
 push!(LOAD_PATH, joinpath(homedir(), "Documents", "julia"))
 
-# try
-#     using OhMyREPL
-# catch e
-#     @warn(e.msg)
-# end
-
 try
     using Revise
 catch e
     @warn(e.msg)
 end
-
