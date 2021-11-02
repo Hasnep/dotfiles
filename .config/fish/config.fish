0a1aaf7c5beb8c044bdffffad28e228d9a075571
Hannes <h@nnes.dev>
2021-11-02 00:53:25 +0000

    Move terminal startup message to fish greeting

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,13 +1,3 @@
-begin
-    set -l terminal_emulator (ps -p (ps -p $fish_pid -o ppid= | tr -d \[:space:\]) -o args=)
-    switch $terminal_emulator
-        case tilix alacritty x-terminal-emulator
-            macchina
-        case "*code*"
-            git rev-parse --git-dir &>/dev/null && onefetch
-    end
-end
-
 set -xg EDITOR micro
 set -xg nvm_default_version lts
 
---
+++ .config/fish/functions/fish_greeting.fish
@@ -0,0 +1,11 @@
+function fish_greeting
+    begin
+        set -l terminal_emulator (ps -p (ps -p $fish_pid -o ppid= | tr -d \[:space:\]) -o args=)
+        switch $terminal_emulator
+            case tilix alacritty x-terminal-emulator
+                macchina
+            case "*code*"
+                git rev-parse --git-dir &>/dev/null && onefetch
+        end
+    end
+end
