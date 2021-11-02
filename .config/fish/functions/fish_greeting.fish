31be49236a74ac1b9e016aab2f0df3f6c4c9ac64
Hannes <h@nnes.dev>
2021-11-02 01:48:40 +0000

    Set ascii art in fish greeting

--- .config/fish/functions/fish_greeting.fish
+++ .config/fish/functions/fish_greeting.fish
@@ -3,7 +3,7 @@ function fish_greeting
         set -l terminal_emulator (ps -p (ps -p $fish_pid -o ppid= | tr -d \[:space:\]) -o args=)
         switch $terminal_emulator
             case tilix alacritty x-terminal-emulator
-                macchina
+                macchina --custom-ascii "~/.config/macchina/ascii/fish.txt"
             case "*code*"
                 git rev-parse --git-dir &>/dev/null && onefetch
         end
