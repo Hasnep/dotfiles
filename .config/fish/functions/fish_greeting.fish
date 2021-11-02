e733f759970df4b9b3731941e35a488c8084b039
Hannes <h@nnes.dev>
2021-11-02 17:04:37 +0000

    Add fixes for MacOS

--- .config/fish/functions/fish_greeting.fish
+++ .config/fish/functions/fish_greeting.fish
@@ -2,10 +2,10 @@ function fish_greeting
     begin
         set -l terminal_emulator (ps -p (ps -p $fish_pid -o ppid= | tr -d \[:space:\]) -o args=)
         switch $terminal_emulator
-            case tilix alacritty x-terminal-emulator
+            case "*alacritty*" x-terminal-emulator
                 macchina --custom-ascii "~/.config/macchina/ascii/fish.txt"
-            case "*code*"
-                git rev-parse --git-dir &>/dev/null && onefetch
+            case "*code*" "*Code*"
+                git rev-parse --git-dir &> /dev/null && onefetch
         end
     end
 end
