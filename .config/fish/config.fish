3d51964a81924531a2c400e96f30b4d42ef3c668
Hannes <h@nnes.dev>
2021-11-04 22:43:50 +0000

    Switch from pure prompt to hydro

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,13 @@
+# Prompt colours
+set --global hydro_color_duration yellow
+set --global hydro_color_error red
+set --global hydro_color_git brblack
+set --global hydro_color_prompt magenta
+set --global hydro_color_pwd cyan
+
+# Environment variables
 set -xg EDITOR micro
 set -xg nvm_default_version lts
 
+# Path
 fish_add_path $HOME/.local/bin/ $HOME/bin/ $HOME/.cargo/bin/ $HOME/.nimble/bin/
