6e68b7d1ef85dab0f245f433e283a032e6086f84
Hannes <h@nnes.dev>
2021-12-19 02:54:13 +0000

    Add support for poetry and local venvs to activate function

--- .config/fish/functions/activate.fish
+++ .config/fish/functions/activate.fish
@@ -1,3 +1,24 @@
-function activate -a "VENV" -d "Activate a Python venv in ~/.venvs/<venv>"
-    source ~/.venvs/$VENV/bin/activate.fish
+function activate --description "Activate a Python venv"
+    # Use Poetry
+    if test -e ./pyproject.toml
+        set venv_path (poetry env info --path)
+
+        # Use venv in project firectory
+    else if test -d ./.venv
+        set venv_path realpath "./.venv"
+    else if test -d ./venv
+        set venv_path realpath "./venv"
+
+        # Use venv in ~/.venvs/
+    else
+        if test (count $argv) = 0
+            set venv_name (basename (pwd))
+        else
+            set venv_name $argv[1]
+        end
+        set venv_path $HOME/.venvs/$venv_name
+    end
+
+    echo "Activating venv in $venv_path"
+    source $venv_path/bin/activate.fish
 end
