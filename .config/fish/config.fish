8f33e0c4ee02e5644755d0d4a8eec885b49cdeb3
Hannes <h@nnes.dev>
2022-02-17 23:25:34 +0000

    Add go bin to path

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -10,4 +10,4 @@ set --export --global EDITOR micro
 set --global nvm_default_version lts
 
 # Path
-fish_add_path $HOME/.local/bin/ $HOME/bin/ $HOME/.cargo/bin/ $HOME/.nimble/bin/ $HOME/.juliaup/bin/
+fish_add_path $HOME/.local/bin/ $HOME/bin/ $HOME/.cargo/bin/ $HOME/.nimble/bin/ $HOME/go/bin/ $HOME/.juliaup/bin/
