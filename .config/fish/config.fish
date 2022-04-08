d7857aaafe3bb521268b7eaa7621e91c18e0425a
Hannes <h@nnes.dev>
2022-04-08 21:20:18 +0100

    Add fzf keybinds

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,6 +6,9 @@ set --global hydro_color_prompt magenta
 set --global hydro_color_pwd cyan
 set --global hydro_multiline true
 
+# Configure fzf plugin keybindings
+fzf_configure_bindings --directory=\cf --git_log= --git_status=\cg --history=\cr --variables --processes
+
 # Environment variables
 set --export --global EDITOR micro
 set --global nvm_default_version lts
