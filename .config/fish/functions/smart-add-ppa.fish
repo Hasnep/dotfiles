4718c3e1a743e5433ab26f5b93e40b0218895883
Hannes <h@nnes.dev>
2022-04-08 21:07:04 +0100

    Clean up quotes

--- .config/fish/functions/smart-add-ppa.fish
+++ .config/fish/functions/smart-add-ppa.fish
@@ -1,4 +1,4 @@
-function smart-add-ppa -a "PPA_URL" -d "Remove a PPA and re-add it"
+function smart-add-ppa -a PPA_URL -d "Remove a PPA and re-add it"
     smart-remove-ppa $PPA_URL
     echo "Adding $PPA_URL"
     sudo add-apt-repository -y $PPA_URL
--- .config/fish/functions/smart-remove-ppa.fish
+++ .config/fish/functions/smart-remove-ppa.fish
@@ -1,4 +1,4 @@
-function smart-remove-ppa -a "PPA_URL" -d "Remove a PPA"
+function smart-remove-ppa -a PPA_URL -d "Remove a PPA"
     echo "Removing $PPA_URL"
     sudo add-apt-repository --remove -y $PPA_URL
 end
