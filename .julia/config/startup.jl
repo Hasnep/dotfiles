6b1a9297a49697a9c3dd861b44d5681b7c90900f
Hannes <h@nnes.dev>
2022-05-12 18:51:00 +0100

    Add nice messages to Julia startup script

--- .julia/config/startup.jl
+++ .julia/config/startup.jl
@@ -4,10 +4,22 @@ try
     using Revise
 catch e
     @warn(e.msg)
+    @info "Command to install Revise:"
+    @info "import Pkg; Pkg.add(\"Revise\")."
 end
 
 try
     using AbbreviatedStackTraces
 catch e
     @warn(e.msg)
+    @info "Command to install AbbreviatedStackTraces:"
+    @info "import Pkg; Pkg.add(url=\"https://github.com/BioTurboNick/AbbreviatedStackTraces.jl\")"
+end
+
+try
+    using JuliaFormatter
+catch e
+    @warn(e.msg)
+    @info "Command to install JuliaFormatter:"
+    @info "import Pkg; Pkg.add(\"JuliaFormatter\")"
 end
