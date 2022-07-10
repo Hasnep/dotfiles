894ab3e6a8213c650e3d341490763a2694922dab
Hannes <h@nnes.dev>
2022-07-10 20:22:47 +0100

    Improve activate function

--- .config/fish/functions/activate.fish
+++ .config/fish/functions/activate.fish
@@ -1,17 +1,18 @@
 function activate --description "Activate a Python venv"
-    # Use Poetry
-    if test -e ./pyproject.toml
-        set venv_path (poetry env info --path)
-
-        # Use venv in project firectory
-    else if test -d ./.venv
+    # Use venv in project directory
+    if test -d ./.venv
         set venv_path (realpath "./.venv")
     else if test -d ./venv
         set venv_path (realpath "./venv")
 
+        # Use Poetry
+    else if test -e ./pyproject.toml
+        set venv_path (poetry env info --path)
+
         # Use venv in ~/.venvs/
     else
         if test (count $argv) = 0
+            # If the name of the venv wasn't specified then try using the name of the current directory
             set venv_name (basename (pwd))
         else
             set venv_name $argv[1]
@@ -19,6 +20,12 @@ function activate --description "Activate a Python venv"
         set venv_path $HOME/.venvs/$venv_name
     end
 
-    echo "Activating venv in $venv_path"
+    # If the virtual environment doesn't exist then raise and error
+    if not test -d $venv_path
+        echo "Could not find a virtual environment at `$venv_path`."
+        return 1
+    end
+    
+    echo "Activating venv in `$venv_path`."
     source $venv_path/bin/activate.fish
 end
