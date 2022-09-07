00a9ec16daa507d1d6a3ae6ff634002393b7808f
Hannes <h@nnes.dev>
2022-09-07 01:50:51 +0100

    Move julia startup file

---
+++ .local/share/julia/config/startup.jl
@@ -0,0 +1,32 @@
+push!(LOAD_PATH, joinpath(homedir(), "Documents", "julia"))
+
+install_instructions = []
+
+try
+    using Revise
+catch e
+    push!(install_instructions, "Pkg.add(\"Revise\")")
+end
+
+try
+    using AbbreviatedStackTraces
+catch e
+    push!(install_instructions, "Pkg.add(url=\"https://github.com/BioTurboNick/AbbreviatedStackTraces.jl\")")
+end
+
+try
+    using JuliaFormatter
+catch e
+    push!(install_instructions, "Pkg.add(\"JuliaFormatter\")")
+end
+
+try
+    using CuratedSystemImages
+catch e
+    push!(install_instructions, "Pkg.add(url=\"https://github.com/MichaelHatherly/CuratedSystemImages.jl\")")
+end
+
+if length(install_instructions) > 0
+    @info "Install missing startup packages:"
+    @info join(["import Pkg", install_instructions...], "; ")
+end
