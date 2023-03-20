0f0dc293482d212ead10c949af10534ce50fb01e
Hannes <h@nnes.dev>
2023-03-20 11:38:44 +0800

    Add hacky script to convert bash exports to fish exports

---
+++ scripts/bash-exports-to-fish.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import sys
+import re
+
+for line in sys.stdin:
+    if line.startswith("export"):
+        match = re.match(r"export (.+)=\"(.+)\"", line)
+        if match:
+            var = match.group(1)
+            value = match.group(2)
+            if var == "PATH":
+                values = value.split(":")
+                for value in values:
+                    print(f"fish_add_path --move {value}")
+            else:
+                print(f"set --export {var} {value}")
