2d211c958c6542e1604e98950ed1fe9e3a99a409
Hannes <h@nnes.dev>
2023-03-20 11:32:14 +0800

    Make fish greeting more robust

--- .config/fish/functions/fish_greeting.fish
+++ .config/fish/functions/fish_greeting.fish
@@ -1,11 +1,17 @@
 function fish_greeting
     begin
-        set -l terminal_emulator (ps -p (ps -p $fish_pid -o ppid= | tr -d \[:space:\]) -o args=)
-        switch $terminal_emulator
-            case "*alacritty*" x-terminal-emulator
-                macchina
-            case "*code*" "*Visual Studio Code.app*"
-                git rev-parse --git-dir &>/dev/null && onefetch
+        if type -q ps
+            set -l terminal_emulator (test $TERM = alacritty && echo alacritty || echo (ps -p (ps -p $fish_pid -o ppid= | tr -d \[:space:\]) -o args=))
+            switch $terminal_emulator
+                case alacritty
+                    if type -q macchina
+                        macchina
+                    end
+                case "*code*" "*Visual Studio Code.app*"
+                    if type -q git; and type -q onefetch
+                        git rev-parse --git-dir &>/dev/null && onefetch
+                    end
+            end
         end
     end
 end
