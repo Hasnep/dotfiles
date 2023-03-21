99e76befda66f390f67af6417c49bb938c76dfc9
Hannes <h@nnes.dev>
2023-03-21 17:05:14 +0800

    Add mkdir to symlink scripts script

--- scripts/symlink-scripts.sh
+++ scripts/symlink-scripts.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
+destination_folder=$HOME/.local/bin
+echo "Creating '$destination_folder' if it doesn't exist."
+mkdir -p "$destination_folder"
+
 find "$HOME/scripts" -type f | while read -r script
 do
     echo "Marking '$script' as executable."
     chmod +x "$script"
-    destination="$HOME/.local/bin/$(basename "$script" | cut -d '.' -f 1)"
+    destination="$destination_folder/$(basename "$script" | cut -d '.' -f 1)"
     echo "Symlinking '$script' to '$destination'."
     ln -s -f "$script" "$destination"
 done
