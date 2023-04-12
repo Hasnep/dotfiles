b11ad5d0afc2d5e3e7ebf8c6a8bff08aba18e6e4
Hannes <h@nnes.dev>
2023-04-12 22:00:02 +0800

    Add guix activation to fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -30,3 +30,11 @@ fish_add_path \
 if type --query --no-functions trash
     abbr --global --add rip 'trash put'
 end
+
+# Activate Guix
+if type --query --no-functions guix
+    set activate_guix_script_path $XDG_CONFIG_HOME/guix-profiles/activate-guix.fish
+    if test -f $activate_guix_script_path
+        source $XDG_CONFIG_HOME/guix-profiles/activate-guix.fish
+    end
+end
