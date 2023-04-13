7589c7f068eb901498106f29bb72dc9557ae8fb6
Hannes <h@nnes.dev>
2023-04-13 23:19:37 +0800

    Speed up fish greeting

--- .config/fish/functions/fish_greeting.fish
+++ .config/fish/functions/fish_greeting.fish
@@ -1,16 +1,15 @@
 function fish_greeting
-    begin
-        if type -q ps
-            set -l terminal_emulator (test $TERM = alacritty && echo alacritty || echo (ps -p (ps -p $fish_pid -o ppid= | tr -d \[:space:\]) -o args=))
-            switch $terminal_emulator
-                case alacritty
-                    if type -q macchina
-                        macchina
-                    end
-                case "*code*" "*Visual Studio Code.app*"
-                    if type -q git; and type -q onefetch
-                        git rev-parse --git-dir &>/dev/null && onefetch
-                    end
+    if test $TERM = alacritty
+        if type --query --no-functions macchina
+            macchina
+        end
+    else
+        if type --query --no-functions ps and type --query --no-functions tail
+            set terminal_emulator (basename "/"(ps -o cmd -f -p (cat /proc/(echo %self)/stat | string split --fields 4 " ") | tail -1 | string split --fields 1 " "))
+            if test $terminal_emulator = code -o $terminal_emulator = codium
+                if type --query --no-functions git and type --query --no-functions onefetch
+                    git rev-parse --git-dir &>/dev/null && onefetch
+                end
             end
         end
     end
