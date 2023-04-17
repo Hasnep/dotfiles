d5fd8767129e22502705c2b5929bc60d5d2cc238
Hannes <h@nnes.dev>
2023-04-17 08:23:26 +0800

    Remove hydro configuration

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,11 +1,3 @@
-# Prompt colours
-set --global hydro_color_duration yellow
-set --global hydro_color_error red
-set --global hydro_color_git brblack
-set --global hydro_color_prompt magenta
-set --global hydro_color_pwd cyan
-set --global hydro_multiline true
-
 # Configure fzf plugin keybindings
 fzf_configure_bindings --directory=\cf --git_log= --git_status=\cg --history=\cr --variables --processes
 
