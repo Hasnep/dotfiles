cfcc754eea09c4351003f379c1b549e44559c3e0
Hannes <h@nnes.dev>
2023-05-02 13:38:29 +0800

    Update OCR script to work on Wayland

--- .config/fish/functions/ocr.fish
+++ .config/fish/functions/ocr.fish
@@ -1,9 +1,20 @@
 function ocr --description="OCR a selection" --argument-names="language"
     set current_datetime (date "+%Y-%m-%d-%H-%M-%S")
-    set temporary_screenshot "/tmp/ocr-screenshot-$current_datetime.png"
+    set temporary_screenshot_directory /tmp/ocr
+    set temporary_screenshot "$temporary_screenshot_directory/$current_datetime.png"
+
+    mkdir -p $temporary_screenshot_directory
 
     # Take the screenshot
-    import $temporary_screenshot
+    switch $XDG_SESSION_TYPE
+        case wayland
+            flameshot gui --path $temporary_screenshot
+        case x11
+            import $temporary_screenshot
+        case '*'
+            echo "Could not detect session type."
+            return 1
+    end
 
     # If you didn't specify a language, ask for the language
     if not test -n "$language"
