bb7f09ce2ef4adba7bf9ebbe3955076309d4f00f
Hannes <h@nnes.dev>
2023-05-06 11:36:46 +0800

    Update greeting

--- .config/fish/functions/fish_greeting.fish
+++ .config/fish/functions/fish_greeting.fish
@@ -1,16 +1,29 @@
 function fish_greeting
-    if test $TERM = alacritty -o $TERM = foot
-        if type --query --no-functions macchina
-            macchina
-        end
-    else
-        if type --query --no-functions ps and type --query --no-functions tail
-            set terminal_emulator (basename "/"(ps -o cmd -f -p (cat /proc/(echo %self)/stat | string split --fields 4 " ") | tail -1 | string split --fields 1 " "))
-            if test $terminal_emulator = code -o $terminal_emulator = codium
+    if type --query --no-functions ps and type --query --no-functions tail
+        set terminal_emulator (basename "/"(ps -o cmd -f -p (cat /proc/(echo %self)/stat | string split --fields 4 " ") | tail -1 | string split --fields 1 " "))
+        switch $terminal_emulator
+            case login
+                return
+            case alacritty foot
+                if type --query --no-functions macchina
+                    macchina
+                    return
+                else
+                    echo "Command 'macchina' not found."
+                end
+            case code codium
                 if type --query --no-functions git and type --query --no-functions onefetch
                     git rev-parse --git-dir &>/dev/null && onefetch
+                    return
+                else
+                    echo "Commands 'git' and 'onefetch' required."
                 end
-            end
+            case '*'
+                # echo "Terminal emulator '$terminal_emulator' is not supported by greeting."
+                return
         end
+    else
+        echo "Commands 'ps' and 'tail' required for greeting."
+        return
     end
 end
