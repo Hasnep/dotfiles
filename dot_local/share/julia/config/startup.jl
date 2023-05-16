07e69f23eba8378748f13db9065a87b063079401
Hannes <h@nnes.dev>
2023-05-16 16:55:09 +0800

    Remove Julia startup packages

--- dot_local/share/julia/config/startup.jl
+++ dot_local/share/julia/config/startup.jl
@@ -8,18 +8,6 @@ catch e
     push!(install_instructions, "Pkg.add(\"Revise\")")
 end
 
-try
-    using AbbreviatedStackTraces
-catch e
-    push!(install_instructions, "Pkg.add(url=\"https://github.com/BioTurboNick/AbbreviatedStackTraces.jl\")")
-end
-
-try
-    using CuratedSystemImages
-catch e
-    push!(install_instructions, "Pkg.add(url=\"https://github.com/MichaelHatherly/CuratedSystemImages.jl\"); import CuratedSystemImages; CuratedSystemImages.install()")
-end
-
 if length(install_instructions) > 0
     @info "Install missing startup packages:"
     @info join(["import Pkg", install_instructions...], "; ")
