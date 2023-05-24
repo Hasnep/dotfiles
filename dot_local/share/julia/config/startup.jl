0d9d58a3a69660f6cf8c935585f4ffff0f1eee6d
Hannes <h@nnes.dev>
2023-05-24 10:08:12 +0800

    Add JuliaFormatter to startup

--- dot_local/share/julia/config/startup.jl
+++ dot_local/share/julia/config/startup.jl
@@ -8,6 +8,12 @@ catch e
     push!(install_instructions, "Pkg.add(\"Revise\")")
 end
 
+try
+    import JuliaFormatter
+catch e
+    push!(install_instructions, "Pkg.add(\"JuliaFormatter\")")
+end
+
 if length(install_instructions) > 0
     @info "Install missing startup packages:"
     @info join(["import Pkg", install_instructions...], "; ")
