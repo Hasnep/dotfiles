5384cb21160bf5261f28f2fa0e2765395e6354c9
Hannes <h@nnes.dev>
2023-06-21 11:12:16 +0800

    Change how environment variables are defined

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -6,18 +6,6 @@ else
     echo "Environment variables script not found at '$environment_variables_script'."
 end
 
-# Guix
-if not status is-login
-    if type --query --no-functions guix
-        set guix_activate_script $XDG_CONFIG_HOME/guix/activate-guix.fish
-        if test -f $guix_activate_script
-            source $guix_activate_script
-        else
-            echo "Guix activation script not found at '$guix_activate_script'."
-        end
-    end
-end
-
 # Add abbreviations
 if type --query --no-functions trash
     abbr --global --add rip 'trash put'
